7dedf758f09a8eb265856059a9666d7572ea910c
Hien Tran <tranthanhhien5@gmail.com>
2021-10-18 15:11:09 +0700

    upload initial project

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+NotFound.propTypes = {};
+
+function NotFound(props) {
+  return <div>Huhu....Not found</div>;
+}
+
+export default NotFound;
